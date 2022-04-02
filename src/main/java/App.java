14180ab4d80058a80dada81b4e9f2638757e9530
lexbekker <Cah4ec182!>
2022-04-02 18:50:54 +0700

    done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,11 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld helloWorld = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat myCat = (Cat) applicationContext.getBean("cat");
+        Cat yourCat = (Cat) applicationContext.getBean("cat");
+        System.out.println("Is HW beans = one bean? " + (bean == helloWorld));
+        System.out.println("Is cat beans = one bean? " + (myCat == yourCat));
+
     }
 }
